0db87d512b2a813f59c07b069933ed20763dcd58
RuvenPinkhasov <rubi00010@gmail.com>
2023-12-29 13:05:04 -0500

    added computer selection logic

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,10 @@ let computerSelection = "";
 let userSelection = "";
 
 let computerRandomNumber = Math.random();
+computerRandomNumber <= 0.33
+  ? (computerSelection = "S")
+  : computerRandomNumber >= 0.67
+  ? (computerSelection = "R")
+  : (computerSelection = "P");
+
+console.log(computerRandomNumber, computerSelection);
