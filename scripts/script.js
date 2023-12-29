05758af4d65b4702722ada1be96ed2b6969aac68
RuvenPinkhasov <rubi00010@gmail.com>
2023-12-29 13:22:12 -0500

    added query selectors for all buttons and game itself

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 let computerSelection = "";
 let userSelection = "";
 
-let computerRandomNumber = Math.random();
-computerRandomNumber <= 0.33
-  ? (computerSelection = "S")
-  : computerRandomNumber >= 0.67
-  ? (computerSelection = "R")
-  : (computerSelection = "P");
+function computerRandom() {
+  let computerRandomNumber = Math.random();
+  computerRandomNumber <= 0.33
+    ? (computerSelection = "S")
+    : computerRandomNumber >= 0.67
+    ? (computerSelection = "R")
+    : (computerSelection = "P");
+}
 
-console.log(computerRandomNumber, computerSelection);
+const wholeGame = document.querySelector(".game");
+const rockButton = wholeGame.querySelector(".game__button_type_rock");
+const paperButton = wholeGame.querySelector(".game__button_type_paper");
+const scissorsButton = wholeGame.querySelector(".game__button_type_scissors");
